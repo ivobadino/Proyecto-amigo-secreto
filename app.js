e214825bc1f9b435c3f3b77373fbdc9ec36d1a9c
ivan badino <badinoivan@gmail.com>
2025-01-29 21:25:56 -0300

    creacion de la estructura basica del codigo

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let nombre = '';
+
+function agregarAmigoAlListado(){
+   
+    nombre = document.getElementById('amigo').value;                //obtener el texto de la caja de carga
+    
+if (nombre == ''){                                                  //chequear si el texto esta vacio
+    //mostrar texto de invalidez
+        
+    }
+    else{                                                           //actualizar el array 
+        agregarAmigo.push(nombre);
+    }
+    
+    limpiarCaja();                                                  //limpiar campo de entrada
+    return;
+}
+
+function actualizarListaMostrada(){
+    //obtener  el elemento de la lista
+    //limpiar el elemento de la lista
+    //iterar sobre el arreglo
+    //agregar elementos a la lista
+
+}
+
+function seleccionarAmigos(){
+    //comprobar si hay elementos en la lista
+    //seleccionar un amigo
+    //obtener el nombre sorteado
+    //mostrar el resultado
+}
+function sortearAmigo(){
+    //validar si lista contiene elementos
+    //crear un indice aleatorio
+    //seleccionar el elemento con ese indice 
+    //obtener el elemento sorteado
+    //mostrar el elemento sorteado
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
