d6f10ba1d8a7acb8d6764771bbbe9196b50464d0
ivan badino <badinoivan@gmail.com>
2025-01-30 22:28:21 -0300

    se agregaron todas las funciones correspondientes

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
 let listaAmigos = [];
 let nombre = '';
+let amigoSorteado = '';
+let amigo = '';
+let ListaDOM = '';
 
 function agregarAmigo(){
    
-    nombre = document.getElementById('amigo').value;                //obtener el texto de la caja de carga
+nombre = document.getElementById('amigo').value;                        //obtener el texto de la caja de carga
     
-    if (nombre == ''){                                              //chequear si el texto esta vacio
-    //mostrar texto de invalidez
-        alert('Por favor, ingrese un nombre valido');
-        return;
+    if (nombre == ''){                                                  //chequear si el texto esta vacio
+        alert('Por favor, ingrese un nombre valido');                   //mostrar texto de invalidez
     }
-    else{                                                           //actualizar el array 
+    else{                                                               //actualizar el array 
         listaAmigos.push(nombre);
         limpiarCaja();
-    }                                                               //limpiar campo de entrada
-    console.log(listaAmigos);
+    }                                                                   //limpiar campo de entrada
+    actualizarListaMostrada();
+    return;                                                             //mostrar la lista por consola
 }
 
 function actualizarListaMostrada(){
-    //obtener  el elemento de la lista
-    //limpiar el elemento de la lista
-    //iterar sobre el arreglo
-    //agregar elementos a la lista
-
+    limpiarTextos();
+    let listaHTML = "";
+    for (let indice = 0; indice < listaAmigos.length; indice++){
+        listaHTML += `<li>${listaAmigos[indice]}</li>`;
+    ListaDOM.innerHTML=listaHTML;
+    }
 }
 
-function seleccionarAmigos(){
-    //comprobar si hay elementos en la lista
-    //seleccionar un amigo
-    //obtener el nombre sorteado
-    //mostrar el resultado
-}
 function sortearAmigo(){
-    //validar si lista contiene elementos
-    //crear un indice aleatorio
-    //seleccionar el elemento con ese indice 
-    //obtener el elemento sorteado
-    //mostrar el elemento sorteado
+    limpiarTextos();
+    if (listaAmigos.length == 0){                                       //comprobar si hay elementos en la lista
+        alert('¡No haz añadido ningun amigo a tu lista!, agrega amigos para hacer el sorteo');
+        return;
+    }
+    else{
+        const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        const ResultadoDOM = document.getElementById('resultado');
+        ResultadoDOM.innerHTML = `<li>El amigo sorteado es: ${listaAmigos[indiceAleatorio]}</li>` ;//mostrar el nombre sorteado
+    } 
 }
 
-function limpiarCaja() {
+function limpiarTextos(){                                               //limpiar todos los textos de etiqueta'<li>'
+    ListaDOM = document.getElementById('resultado');
+    ListaDOM.innerHTML = '';
+    ListaDOM = document.getElementById('listaAmigos');   
+    ListaDOM.innerHTML = '';
+}
+
+function limpiarCaja() {                                                //limpiar caja
     document.querySelector('#amigo').value = '';
     nombre.value = '';
 }
+
+
